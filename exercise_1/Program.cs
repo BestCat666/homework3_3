d4f1948b05f12bbdd7f9325e7cdab9a646a364eb
Best Cat <kotova-irina11@rambler.ru>
2022-09-07 21:42:19 +0300

    решение задачи 19

--- exercise_1/Program.cs
+++ exercise_1/Program.cs
@@ -4,3 +4,23 @@
 //12821 -> да
 //23432 -> да
 
+Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+string stringNumber = Convert.ToString(num);                     
+//Console.Write($"{stringNumber[2]}");
+if (num > 9999 && num < 100000)
+{
+if  (stringNumber[0] == stringNumber[4] && stringNumber[1] == stringNumber[3])
+{
+Console.WriteLine("Введённое Вами число является палиндромом");
+}
+else
+{
+Console.WriteLine("Введённое Вами число НЕ является палиндромом");
+}
+}
+
+
+// 1 2 8 2 1
+// 0 1 2 3 4 индекс
+
