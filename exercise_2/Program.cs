7cb9aeaf4b65eabf2c67247ee4e4d261eed6b868
Best Cat <kotova-irina11@rambler.ru>
2022-09-07 20:43:32 +0300

    решение задачи 21

--- exercise_2/Program.cs
+++ exercise_2/Program.cs
@@ -4,18 +4,18 @@
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.WriteLine("Введите координаты первой точки");
 Console.Write("Введите координату по оси x: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
+int x1 = int.Parse(Console.ReadLine());
 Console.Write("Введите координату по оси y: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
 Console.Write("Введите координату по оси z: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координаты второй точки");
 Console.Write("Введите координату по оси x: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
 Console.Write("Введите координату по оси y: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
 Console.Write("Введите координату по оси z: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
 double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 double roundDistance = Math.Round(distance, 2);
 Console.WriteLine($"Расстояние между двумя точками  равно {roundDistance}");
